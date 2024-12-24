49fdc195bf56907e808fa8f1588ac9c03c3bf835
Yaroslav Kovalevskyi <kovalevskyi96@gmail.com>
2024-12-24 23:46:31 +0200

    added ec2 as wp instance

--- tf/main.tf
+++ tf/main.tf
@@ -11,13 +11,20 @@ module "wordpress" {
   domain_name = "kovalevskyi.space"
   vpc_id      = module.networking.vpc_properties.id
   db_variables = {
-    db_name         = "wordpress"
-    engine          = "MySQL"
-    identifier      = "${var.project}wordpress"
-    instance_class  = "db.t3.micro"
-    master_username = "root"
-    storage         = 30
-    subnet_group    = module.networking.database_subnet_group_properties.id
+    db_name             = "wordpress"
+    engine              = "MySQL"
+    identifier          = "${var.project}wordpress"
+    instance_class      = "db.t3.micro"
+    master_username     = "root"
+    publicly_accessible = true
+    storage             = 30
+    subnet_group        = module.networking.database_subnet_group_properties.id
+  }
+
+  ec2_variables = {
+    instance_type    = "t3.micro"
+    public_subnet_id = module.networking.public_subnets_properties[0].id
+
   }
   wellknown_ip = {
     YK-home = "193.227.206.114/32"
