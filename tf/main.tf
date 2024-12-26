a9f8235004d8d666a8d775745036408a45621ed3
Yaroslav Kovalevskyi <kovalevskyi96@gmail.com>
2024-12-26 18:11:11 +0200

    added vars for service ports and redis

--- tf/main.tf
+++ tf/main.tf
@@ -11,22 +11,35 @@ module "wordpress" {
   domain_name = "kovalevskyi.space"
   vpc_id      = module.networking.vpc_properties.id
   db_variables = {
-    db_name             = "wordpress"
-    engine              = "MySQL"
-    identifier          = "${var.project}wordpress"
-    instance_class      = "db.t3.micro"
-    master_username     = "root"
-    publicly_accessible = true
-    storage             = 30
-    subnet_group        = module.networking.database_subnet_group_properties.id
+    db_name         = "wordpress"
+    engine          = "MySQL"
+    identifier      = "${var.project}wordpress"
+    instance_class  = "db.t3.micro" // free tier
+    master_username = "root"
+    storage         = 30
+    subnet_group    = module.networking.database_subnet_group_properties.id
   }
 
   ec2_variables = {
-    instance_type    = "t3.micro"
+    instance_type    = "t3.micro" // free tier
     public_subnet_id = module.networking.public_subnets_properties[0].id
-
+    wp_admin_email  = "kovalevskyi96@gmail.com"
   }
-  wp_admin_email = "kovalevskyi96@gmail.com"
+  service_ports = {
+    http  = 80
+    https = 443
+    mysql = 3306
+    redis = 6379
+  }
+  redis_variables = {
+    cluster_id           = "${var.project}-redis"
+    engine_version       = "7.1"
+    node_type            = "cache.t3.micro" // free tier
+    num_cache_nodes      = 1
+    parameter_group_name = "default.redis7"
+  }
+  private_subnets = values(module.networking.private_subnets_properties)[*].id // cannot be inside redis_variables. Map must contain same (or convertible) data type for all values (string / list)
+
   wellknown_ip = {
     YK-home = "193.227.206.114/32"
   }
